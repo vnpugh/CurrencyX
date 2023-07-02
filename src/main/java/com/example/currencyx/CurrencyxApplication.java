454063959abc2344c8a0dc5624cacec47e122697
vnpugh <vnp19@yahoo.com>
2023-07-02 14:40:58 -0500

    Initial Commit

---
+++ src/main/java/com/example/currencyx/CurrencyxApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyxApplication.class, args);
+    }
+
+}
